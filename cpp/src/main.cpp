6a9a7c046259f0b32c19810829eee186f1dfaba6
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2023-01-11 14:26:14 -0500

    vectors

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -4,8 +4,18 @@
 #include <iostream>
 #include <filesystem>
 
+#include <vector>
+#include <string>
+
 int main(){
   std::string path = "/etc/rc.d";
-    for (const auto & entry : std::filesystem::directory_iterator(path))
-        std::cout << entry.path() << std::endl;;
+  std::vector<std::string> entries;
+  for (const auto &entry : std::filesystem::directory_iterator(path)) {
+    //std::cout << entry.path() << std::endl;
+    entries.push_back(entry.path());
+  }
+
+  for (long unsigned int i = 0; i < entries.size(); i++) {
+    printf("%lu: %s\n",i,entries.at(i).c_str());
+  }
 }
